abf6ba7d6214aeaf5d1e387bea9124c91a837cfe
Vksharma-72 <kirshansharma3546@gmail.com>
2026-06-01 23:27:19 +0530

    Data Ingestion Pipeline is Complited

---
+++ main.py
@@ -0,0 +1,13 @@
+from textSummarizer.pipeline.data_pipeline import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger 
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<< ")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x==============x ")
+except Exception as e:
+    logger.exception(e)
+    raise e
